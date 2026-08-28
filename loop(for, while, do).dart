65a441dd54966164f32a9743e3d423861486dc21
mahmud <mahmud.mahmud5@gmail.com>
2026-08-29 00:53:49 +0600

    practice sega

--- loop(for, while, do).dart
+++ loop(for, while, do).dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 main(){
   /*for(int a=0; a<=10; a++){
     print("MAHMUD");
   }*/
 
   int b=1;
-  while(b<=11){
+//  print("Enter the value of u want to print:");
+ // b=int.parse(stdin.readLineSync()!);
+  while(5>=b){
     print("MESSI is $b");
-    b++;
+b++;
 
   }
 
